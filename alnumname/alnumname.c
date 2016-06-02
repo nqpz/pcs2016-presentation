92f8c5f2c13cd07e808c9eaee701609b9610cce0
Kasper F. Brandt <poizan@poizan.dk>
2016-06-02 17:12:20 +0200

    This is useful

--- alnumname/alnumname.c
+++ alnumname/alnumname.c
@@ -7,6 +7,12 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+/*static char request[0x6400]; /* TODO: Do we still need this? */
+
+/* Make sure it's large enough for the future */
+static char parsedvalue[65536];
+int get_value(char* keyname, char* req);
+
 int name_error()
 {
     printf("Content-Type: text/html\n\n");
@@ -14,46 +20,47 @@ int name_error()
     return 1;
 }
 
-/* Make sure it's large enough for the future */
-static char postdata[1048576];
+int handle_post(char* postdata)
+{
+    int len;
+    char name[50] = {0}; /* No one has longer names than that*/
+    if (!(len = get_value("name", postdata)))
+        return name_error();
+    memcpy(name, parsedvalue, len); 
 
-int get_name(char* keyname, char* req, char* buf)
+    printf("Content-Type: text/html\n\n");
+    printf("<BODY BACKGROUND=../bg.jpg><H1><FONT><BLINK>WELCOME %s</H1></BLINK>", name);
+}
+
+/* The magic sauce! */
+int get_value(char* keyname, char* req)
 {
     char* key;
-    char* name = 0;
+    char* buf = parsedvalue;
+    char* value = 0;
     while (*req)
     {
         for (key = req; *req && *req != '&'; req++)
         {
             if (*req == '=' && strncmp(key, keyname, strlen(keyname)) == 0)
             {
-                name = req+1;
+                value = req+1;
                 break;
             }
         }
     }
-    if (!name)
+    if (!value)
         return 0;
-    for (; *name && *name != '&'; name++, buf++)
+    for (; *value && *value != '&'; value++, buf++)
     {
-        if ((*name < 'a' || *name > 'z') && (*name < 'A' || *name > 'Z') &&
-            (*name < '0' || *name > '9'))
+        if ((*value < 'a' || *value > 'z') && (*value < 'A' || *value > 'Z') &&
+            (*value < '0' || *value > '9'))
         {
             return 0;
         }
-        *buf = *name;
+        *buf = *value;
     }
-    return 1;
-}
-
-int handle_post()
-{
-    char name[50] = {0}; /* No one has longer names than that*/
-    if (!get_name("name", postdata, name))
-        return name_error();
-
-    printf("Content-Type: text/html\n\n");
-    printf("<H1><BLINK>WELCOME %s</H1></BLINK>", name);
+    return buf - &parsedvalue[0];
 }
 
 int main(int argc, char* argv[])
@@ -61,14 +68,19 @@ int main(int argc, char* argv[])
     char* request_method = getenv("REQUEST_METHOD");
     if (strcmp(request_method, "POST") == 0)
     {
-        int len = read(STDIN_FILENO, postdata, 1048575);
-        return handle_post();
+        char postdata[65536];
+        int len = read(STDIN_FILENO, postdata, 65535);
+        postdata[len] = 0;
+        return handle_post(postdata);
     }
 
     printf("Content-Type: text/html\n\n");
     printf("<HTML><HEAD><TITLE>Totally Secure Form</TITLE>");
-    printf("<BODY><FORM ACTION=alnumname METHOD=POST>");
+    printf("<BODY BACKGROUND=../bg.jpg><FORM ACTION=alnumname METHOD=POST>");
     printf("Enter nick: <INPUT TYPE=TEXT NAME=name><br>");
     printf("<INPUT TYPE=SUBMIT VALUE=OK>");
+    printf("<HR>");
+    printf("<SMALL>Copyright (C) 1995 Karl Koder");
 }
+
 
--- alnumname/build.sh
+++ alnumname/build.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-gcc -m32 -Wl,-z,execstack -Wl,-z,norelro -fno-stack-protector alnumname.c -o alnumname
+gcc -m32 -Wl,-z,execstack -Wl,-z,norelro -Wl,-Ttext-segment=0x8000000 -fno-stack-protector alnumname.c -o alnumname
