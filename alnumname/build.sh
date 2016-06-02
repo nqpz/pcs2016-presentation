b79fccec80a2ecf7fc0939fbbf427f2c5db930c2
Kasper F. Brandt <poizan@poizan.dk>
2016-06-02 17:35:15 +0200

    update build script and and background image

--- alnumname/build.sh
+++ alnumname/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-gcc -m32 -Wl,-z,execstack -Wl,-z,norelro -Wl,-Ttext-segment=0x8000000 -fno-stack-protector alnumname.c -o alnumname
+mkdir www/cgi-bin
+gcc -m32 -O0 -Wl,-z,execstack -Wl,-z,norelro -Wl,-Ttext-segment=0x8000000 -fno-stack-protector alnumname.c -o www/cgi-bin/alnumname
